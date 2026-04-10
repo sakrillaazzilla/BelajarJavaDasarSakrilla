592a91ee02289d7edf9c287fda95fd504a308f27
sakrillaazilla <sakrillaazzillahkm@gmail.com>
2026-04-10 13:29:55 +0700

    BelajarJavaDasarSakrilla

---
+++ src/main/java/com/mycompany/belajarjavadasarsakrilla/BelajarJavaDasarSakrilla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajarjavadasarsakrilla;
+
+/**
+ *
+ * @author HP
+ */
+public class BelajarJavaDasarSakrilla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
